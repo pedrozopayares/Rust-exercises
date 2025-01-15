fb8f72f522c608fd082dd44f6f47c154fa38407d
Javier Pedrozo <7519384+pedrozopayares@users.noreply.github.com>
2025-01-15 10:05:04 -0500

    Hello world

---
+++ rust/lesson_0/target/debug/deps/lesson_0-2bde7f195bfa00d5.d
@@ -0,0 +1,5 @@
+/Users/pedrozopayares/Development/Rust/Rust-exercises/rust/lesson_0/target/debug/deps/lesson_0-2bde7f195bfa00d5: src/main.rs
+
+/Users/pedrozopayares/Development/Rust/Rust-exercises/rust/lesson_0/target/debug/deps/lesson_0-2bde7f195bfa00d5.d: src/main.rs
+
+src/main.rs:
---
+++ rust/lesson_0/target/debug/lesson_0.d
@@ -0,0 +1,1 @@
+/Users/pedrozopayares/Development/Rust/Rust-exercises/rust/lesson_0/target/debug/lesson_0: /Users/pedrozopayares/Development/Rust/Rust-exercises/rust/lesson_0/src/main.rs
